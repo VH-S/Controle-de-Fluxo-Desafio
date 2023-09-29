283bd485f7bf09c2cac026172c43bb0e1ec0ea5c
VH-S <116882856+VH-S@users.noreply.github.com>
2023-09-29 07:04:30 -0300

    .

---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro.");
+    }
+
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
